61859f33f62114f1888f4463b5da413a9a6cd285
Maojin-juan <edwardelric199887@gmail.com>
2024-06-04 23:47:21 +0800

    首頁標籤權重調整、卡片內容更正，作品集手機版逗點置中

--- js/public/slideshow.dev.js
+++ js/public/slideshow.dev.js
@@ -2,9 +2,7 @@
 
 var swiper = new Swiper('.swiper', {
   autoplay: {
-    // 自動輪播 swiper
-    delay: 3 * 1000 // 每兩秒切換下一張
-
+    delay: 3 * 1000
   },
   grid: {
     rows: 3
@@ -18,7 +16,6 @@ var swiper = new Swiper('.swiper', {
       spaceBetween: 24
     }
   },
-  //分頁
   pagination: {
     el: '.swiper-pagination',
     clickable: true
--- js/slideshow.js
+++ js/slideshow.js
@@ -1,6 +1,6 @@
 var swiper = new Swiper('.swiper', {
-  autoplay: { // 自動輪播 swiper
-    delay: 3 * 1000, // 每兩秒切換下一張
+  autoplay: {
+    delay: 3 * 1000,
   },
   grid: {
     rows: 3,
@@ -14,7 +14,6 @@ var swiper = new Swiper('.swiper', {
       spaceBetween: 24,
     }
   },
-  //分頁
   pagination: {
       el: '.swiper-pagination',
       clickable: true
