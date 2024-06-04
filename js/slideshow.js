a2fc1aa8bbf1995b0bef253ca5bb095aacdb05b9
Maojin-juan <edwardelric199887@gmail.com>
2024-06-04 11:03:37 +0800

    slideshow功能正常運作，但切版有問題，整個swiper-slide會滿版

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,9 +1,10 @@
-const swiper = new Swiper('.swiper', {
-  slidesPerView: 3,
+var swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
   spaceBetween: 24,
-  // 分頁   
+  //分頁
   pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+      el: '.swiper-pagination',
+      clickable: true,
   },
+  loop: true
 });
